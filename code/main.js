377e746e707f8f6d3f7670ea7533ee1d4a90daf6
ruy_pa_ <dobaduy.hungyen@gmail.com>
2024-06-11 00:03:35 +0700

    lesson 5

--- code/main.js
+++ code/main.js
@@ -1,1 +1,44 @@
-alert("hi mother fucker")
+
+
+// lesson 2: variable
+var fullName = 'Duy Ba';
+// có chấm phẩy và không có chấm phẩy khác nhau ở đâu ?
+// đơn giản chỉ là nếu viết chung dòng thì nó sẽ không chung được
+// nên dùng dấu ; 
+
+// lession 1
+// alert(fullName);
+
+// lesson 3 built in fuction
+    // 1.alert
+    // 2.console
+    // 3.confirm
+    // 4.prompt
+    // 5.set timepout
+    // 6.set interval
+
+console.log('duy');
+
+// confirm('message');
+
+// prompt('confirm');
+
+// set sau bao lau thi thuc hien ham
+// thuc hien duy nhat 1 lan
+// setTimeout(function () {
+//     alert('vailozluon');
+// }, 1000);
+
+// thang nay thi thuc hien ham sau chu ki thoi gian
+// thuc hien nhieu lan
+// setInterval(function () {
+//     console.log(Math.random());
+// }, 1000)
+
+// lesson 4: toán tử trong js
+// 1. toán tử số học - arithmetic
+// 2. toán tử gán - assignment
+// 3. toán tử so sánh - comparision
+// 4. toán tử logic - logical
+
+// lesson 5: toán tử số học
